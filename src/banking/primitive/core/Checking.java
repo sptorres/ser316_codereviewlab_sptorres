462e182df295ea77a30ce122cdebf131d379a594
Shayne Torres <shaynetorres@Shaynes-MacBook-Pro.local>
2017-04-06 22:22:54 -0700

    move deposit method to account class to avoid duplicate code

--- src/banking/primitive/core/Checking.java
+++ src/banking/primitive/core/Checking.java
@@ -16,22 +16,7 @@ public class Checking extends Account {
 	public Checking(String name, float balance) {
 		super(name, balance);
 	}
-
-	/**
-	 * A deposit may be made unless the Checking account is closed
-	 * @param float is the deposit amount
-	 */
-	public boolean deposit(float amount) {
-		if (getState() != State.CLOSED && amount > 0.0f) {
-			balance = balance + amount;
-			if (balance >= 0.0f) {
-				setState(State.OPEN);
-			}
-			return true;
-		}
-		return false;
-	}
-
+	
 	/**
 	 * Withdrawal. After 10 withdrawals a fee of $2 is charged per transaction You may 
 	 * continue to withdraw an overdrawn account until the balance is below -$100
